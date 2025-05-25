d34b05eb60a544a15a2c61342dd4bca246a3700b
dixon.araya2002 <dixon.araya2002@gmail.com>
2025-05-25 07:27:15 +0000

    otra prueba

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   const { email, password } = body;
 
